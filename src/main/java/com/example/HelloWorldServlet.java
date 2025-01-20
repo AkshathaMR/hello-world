96d82cdc832421b4f16bf2590e19dd09a52955c5
AkshathaMR <48641238+AkshathaMR@users.noreply.github.com>
2025-01-20 21:45:42 +0530

    Update HelloWorldServlet.java

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,8 +1,5 @@
 package com.example;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
